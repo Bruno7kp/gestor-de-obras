315b8b7e140d888e4171d8e5c63726c32d21b44e
bruno <bruno7kp@outlook.com>
2026-02-28 15:36:01 -0300

    feat: add new stock request statuses for partial and full deliveries

--- backend/prisma/migrations/20260228164331_add_partial_stock_deliveries/migration.sql
+++ backend/prisma/migrations/20260228164331_add_partial_stock_deliveries/migration.sql
@@ -1,3 +1,7 @@
+-- AlterEnum: Add PARTIALLY_DELIVERED and DELIVERED to StockRequestStatus
+ALTER TYPE "StockRequestStatus" ADD VALUE 'PARTIALLY_DELIVERED';
+ALTER TYPE "StockRequestStatus" ADD VALUE 'DELIVERED';
+
 -- AlterTable: Add quantityDelivered to StockRequest
 ALTER TABLE "StockRequest" ADD COLUMN "quantityDelivered" DOUBLE PRECISION NOT NULL DEFAULT 0;
 
